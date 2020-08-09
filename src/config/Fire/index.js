8cb84733664bf42bf009500ae0127c7e907bc8de
syaiful919 <syaiful@cls-indo.com>
2020-08-09 10:10:33 +0700

    comment config

--- src/config/Fire/index.js
+++ src/config/Fire/index.js
@@ -1,13 +1,15 @@
 import firebase from 'firebase';
 
+// use your own firebase config
+
 firebase.initializeApp({
-  apiKey: 'AIzaSyA7ekMT4Iq0hZeP2MkLVC4YrCmjmy0vfHs',
-  authDomain: 'punten-doc.firebaseapp.com',
-  databaseURL: 'https://punten-doc.firebaseio.com',
-  projectId: 'punten-doc',
-  storageBucket: 'punten-doc.appspot.com',
-  messagingSenderId: '375977281667',
-  appId: '1:375977281667:web:4173b8e1a45fd65d8fb3b6',
+  // apiKey: 'AIzaSyA7ekMT4Iq0hZeP2MkLVC4YrCmjmy0vfHs',
+  // authDomain: 'punten-doc.firebaseapp.com',
+  // databaseURL: 'https://punten-doc.firebaseio.com',
+  // projectId: 'punten-doc',
+  // storageBucket: 'punten-doc.appspot.com',
+  // messagingSenderId: '375977281667',
+  // appId: '1:375977281667:web:4173b8e1a45fd65d8fb3b6',
 });
 
 const Fire = firebase;
